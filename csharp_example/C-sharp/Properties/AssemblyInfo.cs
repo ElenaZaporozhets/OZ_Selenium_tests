644bd03cc0fff24a6851a33855881ce1914bad4a
ElenaZaporozhets <57036327+ElenaZaporozhets@users.noreply.github.com>
2019-11-07 07:46:16 +0200

    Creation of the first C# project

---
+++ csharp_example/C-sharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("C-sharp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("C-sharp")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b9568239-6815-4bd5-adca-fd68ca52604a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ csharp_example/C-sharp/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace C_sharp
+{
+    [TestFixture]
+
+    public class MyFirstTest
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+
+        public void start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+
+        public void FirstTest ()
+        {
+            driver.Url = "http://wwww.google.com/";
+            driver.FindElement(OpenQA.Selenium.By.Name("q")).SendKeys("webdriver");
+            driver.FindElement(OpenQA.Selenium.By.Name("btnG")).Click();
+            wait.Until(ExpectedConditions.TitleIs("webdriver - search in Google"));
+        }
+
+        [TearDown]
+
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+  }
